9505df4a884d511f96dd5247a9b98a802b7b0231
David Foote <david.a.foote@gmail.com>
2019-06-03 04:57:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,13 +5,19 @@ end
 addition(3, 5)
 
 def subtraction(num1, num2)
-   return num1 + num2
+   return num1 - num2
 end
+
+subtraction(10, 5)
 
 def division(num1, num2)
+  return num1 / num2
 end
 
+division(81, 9)
+
 def multiplication(num1, num2)
+  
 end
 
 def modulo(num1, num2)
